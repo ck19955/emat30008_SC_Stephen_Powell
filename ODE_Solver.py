68a5069f0b0bd4f264aa7df0b705f1e23030e941
Stephen <ck19955@bristol.ac.uk>
2022-03-03 15:41:29 +0000

    Developed euler_step() to compute a single euler step. Also developed solve_to() which solves from x1,t1 to x2,t2.

--- ODE_Solver.py
+++ ODE_Solver.py
@@ -1,11 +1,20 @@
 # Solves ODEs
 
-def euler_step():
-    pass
+def euler_step(function, x_n, t_n, step_size):
+    x_n_1 = x_n + step_size*function(t_n, x_n)
+    return x_n_1
 
 
-def solve_to():
-    pass
+def solve_to(function, x_0, t_0, t_end, deltaT_max):
+    x = x_0
+    t = t_0
+    while t < t_end:
+        if t + deltaT_max <= t_end:
+            x = euler_step(function, x, t, deltaT_max)
+            t = t + deltaT_max
+        else:
+            deltaT_max = t_end - t
+    return x
 
 
 def solve_ode():

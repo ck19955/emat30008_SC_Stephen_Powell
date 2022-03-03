c9161947c3cadff14fcbf2f55ed5252b98fa3394
Stephen <ck19955@bristol.ac.uk>
2022-03-03 17:39:18 +0000

    Added new function, solve_ode(), which is suited to solve ODEs using the euler method. A new function named 'f' has been added as the ode to be solved.

--- ODE_Solver.py
+++ ODE_Solver.py
@@ -1,21 +1,33 @@
 # Solves ODEs
+import math
 
-def euler_step(function, x_n, t_n, step_size):
-    x_n_1 = x_n + step_size*function(t_n, x_n)
+
+def euler_step(x_n, t_n, step_size):
+    x_n_1 = x_n + step_size * f(t_n, x_n)
     return x_n_1
 
 
-def solve_to(function, x_0, t_0, t_end, deltaT_max):
+def solve_to(x_0, t_0, t_end, deltaT_max):
     x = x_0
     t = t_0
     while t < t_end:
         if t + deltaT_max <= t_end:
-            x = euler_step(function, x, t, deltaT_max)
+            x = euler_step(x, t, deltaT_max)
             t = t + deltaT_max
         else:
             deltaT_max = t_end - t
     return x
 
 
-def solve_ode():
-    pass
+def solve_ode(x_0, t_values, deltaT_max):
+    x_values = [0] * len(t_values)
+    x_values[0] = x_0
+    for i in range(len(t_values)-1):
+        x_values[i+1] = solve_to(x_values[i], t_values[i], t_values[i + 1], deltaT_max)
+    return x_values
+
+
+def f(t, x):
+    return math.exp(t)
+
+

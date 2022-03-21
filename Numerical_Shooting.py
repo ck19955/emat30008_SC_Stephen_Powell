28d3071108c3dee6b4701fbc1ac49eebbf091ec5
Stephen <ck19955@bristol.ac.uk>
2022-03-21 14:54:03 +0000

    Added Numerical_Shooting.py using fsolve(). It was able to find the orbit that I had previously isolated.

--- Numerical_Shooting.py
+++ Numerical_Shooting.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from ODE_Solver import *
 from scipy.signal import argrelextrema
+from scipy.integrate import solve_ivp
+from scipy.optimize import fsolve
 
 
 def ode_num(t, x_values, a, b, d):
@@ -27,16 +29,31 @@ def isolate_orbit(ode_data, time_data):
     return
 
 
+def shooting_conditions(u0, args):
+    x0 = u0[:-1]
+    t0 = u0[-1]
+    sol = solve_ivp(ode_num, (0,t0), x0, max_step=1e-2, args=args)
+    x_condition = x0 - sol.y[:,-1]
+    t_condition = np.asarray(ode_num(t0,x0,*args)[0])
+    g_condition = np.concatenate((x_condition, t_condition), axis=None)
+    return g_condition
+
+
 times1 = np.linspace(0, 200, num=1000)
 RK4_values = np.asarray(solve_ode(times1, np.array([1, 1]), 0.1, RK4, ode_num, np.array([1, 0.2, 0.1])))
 print(isolate_orbit(RK4_values, times1))
+args = np.array([1, 0.2, 0.1])
+final = fsolve(shooting_conditions, np.array([1, 1, 20]), args=args)
+print(final)
 
 
+'''
 plt.plot(times1, RK4_values[:, 0])
 plt.plot(times1, RK4_values[:, 1])
 plt.xlabel('time')
 plt.ylabel('u')
 plt.show()
+'''
 
 '''
 fig, axs = plt.subplots(2, 2)

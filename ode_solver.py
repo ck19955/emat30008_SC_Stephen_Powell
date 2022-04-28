03a65f91f98640882f6813d9fab6443feea67359
Stephen <ck19955@bristol.ac.uk>
2022-04-28 23:30:50 +0100

    Added docstrings to each function and commented code

--- ode_solver.py
+++ ode_solver.py
@@ -8,25 +8,25 @@ from function_examples import *
 
 def euler_step(t_n, x_n, step_size, ode, args):
     """
-    Executes a single step of the forward euler method for given value, t_n
+    Executes a single step of the forward euler method for a given value, t_n
 
     Parameters:
     ----------
         t_n : float
             The value of the independent variable
         x_n : numpy array
-            The value of the dependant variable
-        step_size : integer
+            The value of the dependant variable(s)
+        step_size : float
             The step-size of the euler step to be executed
         ode : function
             The ODE for which the euler step predicts
-        args : list
+        args : numpy array
             The parameters of the ODE
 
     Returns:
     ----------
         x : numpy array
-            The new value of the dependant variable after an euler step
+            The new value of the dependant variable(s) after an euler step
     """
 
     x = x_n + step_size * ode(t_n, x_n, *args)
@@ -38,13 +38,23 @@ def rk4(t_n, x_n, step_size, ode, args):
     Executes a single step of the 4th Order Runge Kutta method for given value, t_n
 
     Parameters:
-        t_n         - The value of the independent variable
-        x_n         - The value of the dependant variable
-        step_size   - The step-size of the rk4 to be executed
+    ----------
+        t_n : float
+            The value of the independent variable
+        x_n : numpy array
+            The value of the dependant variable(s)
+        step_size : float
+            The step-size of the rk4 step to be executed
+        ode : function
+            The ODE for which the rk4 step predicts
+        args : numpy array
+            The parameters of the ODE
 
     Returns:
-        x           - The new value of the dependant variable after a rk4 step
-    """
+    ----------
+        x : numpy array
+            The new value of the dependant variable(s) after an rk4 step
+"""
     k1 = ode(t_n, x_n, *args)
     k2 = ode(t_n + step_size/2, x_n + k1*(step_size/2), *args)
     k3 = ode(t_n + step_size/2, x_n + k2*(step_size/2), *args)
@@ -55,25 +65,25 @@ def rk4(t_n, x_n, step_size, ode, args):
 
 def improved_euler_step(t_n, x_n, step_size, ode, args):
     """
-    Executes a single step of the forward euler method for given value, t_n
+    Executes a single step of the improved euler method for given value, t_n
 
     Parameters:
     ----------
         t_n : float
             The value of the independent variable
         x_n : numpy array
-            The value of the dependant variable
-        step_size : integer
-            The step-size of the euler step to be executed
+            The value of the dependant variable(s)
+        step_size : float
+            The step-size of the improved euler step to be executed
         ode : function
-            The ODE for which the euler step predicts
-        args : list
+            The ODE for which the improved euler step predicts
+        args : numpy array
             The parameters of the ODE
 
     Returns:
     ----------
         x : numpy array
-            The new value of the dependant variable after an euler step
+            The new value of the dependant variable(s) after an improved euler step
     """
 
     x = x_n + step_size/2 * (ode(t_n, x_n, *args) + ode(t_n + step_size, x_n + step_size*ode(t_n, x_n, *args), *args))
@@ -82,25 +92,25 @@ def improved_euler_step(t_n, x_n, step_size, ode, args):
 
 def heun_step(t_n, x_n, step_size, ode, args):
     """
-    Executes a single step of the forward euler method for given value, t_n
+    Executes a single step of Heun's method for given value, t_n
 
     Parameters:
     ----------
         t_n : float
             The value of the independent variable
         x_n : numpy array
-            The value of the dependant variable
-        step_size : integer
-            The step-size of the euler step to be executed
+            The value of the dependant variable(s)
+        step_size : float
+            The step-size of a single Heun's method step to be executed
         ode : function
-            The ODE for which the euler step predicts
-        args : list
+            The ODE for which a single Heun's method step predicts
+        args : numpy array
             The parameters of the ODE
 
     Returns:
     ----------
         x : numpy array
-            The new value of the dependant variable after an euler step
+            The new value of the dependant variable(s) after a single Heun's step
     """
 
     k1 = ode(t_n, x_n, *args)
@@ -113,17 +123,27 @@ def heun_step(t_n, x_n, step_size, ode, args):
 
 def solve_to(t_0, t_end, x_0, deltaT_max, method, ode, args):
     """
-    Solves an ODE for an array of values of the independent variable.
+    Solves an ODE for an array of values of the independent variable, t.
 
     Parameters:
-        t_0         - The initial value of the independent variable
-        t_end       - The final value of the independent variable
-        x_0         - The initial value of the dependant variable
-        deltaT_max  - The maximum value the step-size can take when performing a step of a numerical method
-        method      - The numerical method to be used to solve the ODE
+    ----------
+        t_0 : float
+            The initial value of the independent variable
+        t_end : float
+            The final value of the independent variable
+        x_0 : numpy array
+            The value of the dependant variable(s)
+        deltaT_max : float
+            The maximum value the step-size can take when performing a step of a numerical method
+        ode : function
+            The ODE to be integrated
+        args : numpy array
+            The parameters of the ODE
 
     Returns:
-        x           - The final value of the dependant variable
+    ----------
+        x : numpy array
+            The final value of the dependant variable(s)
     """
 
     x = x_0
@@ -144,16 +164,25 @@ def solve_ode(t_values, x_0, deltaT_max, method, ode, args=None):
     between the two values must be smaller than deltaT_max.
 
     Parameters:
-        t_values    - An array of values of the independent variable, for which each t_value should be a assigned an
-                       x value
-        x_0         - The initial value of the dependant variable
-        deltaT_max  - The maximum value the step-size can take when performing a step of a numerical method
-        method      - The numerical method to be used to solve the ODE
+    ----------
+        t_values : array
+            values of the independent variable
+        x_0 : numpy array
+            The initial value of the dependant variable(s)
+        deltaT_max : float
+            The maximum value the step-size can take when performing a step of a numerical method
+        method : function
+            The one-step integration method to be used on the ODE
+        ode : function
+            The ODE to be integrated
+        args : numpy array
+            The parameters of the ODE
 
     Returns:
-        x           - The final value of the dependant variable
+    ----------
+        x : numpy array
+            An array of dependant variable values for the integrated ODE
     """
-
     if args is None:  # args cannot be passed through as None because this induces an error when trying to unpack
         args = []  # This allows for unpacking
 
@@ -164,21 +193,26 @@ def solve_ode(t_values, x_0, deltaT_max, method, ode, args=None):
     return x_values
 
 
-def error_plot(t_values, x_0, ode, exact, args=None):
+def error_plot(t_values, x_0, ode, exact, error_match, args=None):
     """
-    Examines the change in error when varying the step-size for various numerical methods. In
-    addition, it compares the time taken for both the euler method and fourth order runge kutta (rk4 to produce a
-    solution to the ODE with the same magnitude of error.
+    Examines the change in error when varying the step-size for various numerical methods. In addition, it compares the
+    time taken and step size required for both the methods to reach same magnitude of error. Also plots a log-log plot
+    the different methods as the step-size varies.
 
     Parameters:
-        t_values         - The range of values of the independent variable
-        x_0              - The initial value of the dependant variable
-
-    Returns:
-        error_eul           - The array of errors for the euler method at each step-size value
-        error_rk4           - The array of errors for the rk4 method at each step-size value
-        time_eul            - The time take for the euler method to reach an error specified in the function
-        time_rk4            - The time take for the rk4 method to reach an error specified in the function
+    ----------
+        t_values : array
+            values of the independent variable
+        x_0 : numpy array
+            The initial value of the dependant variable(s)
+        ode : function
+            The ODE to be integrated
+        exact : function
+            The exact function which returns the true solution
+        error_match : float
+            The error used to find how long each method takes and the step-size required to reach a specific error
+        args : numpy array
+            The parameters of the ODE
     """
 
     if args is None:  # args cannot be passed through as None because this induces an error when trying to unpack
@@ -192,8 +226,6 @@ def error_plot(t_values, x_0, ode, exact, args=None):
     error_rk4 = np.zeros(len(step_sizes))
     error_improv = np.zeros(len(step_sizes))
     error_heun = np.zeros(len(step_sizes))
-
-    error_match = 1e-6  # The error used to find how long each method takes to reach a specific error.
 
     # Initialise the values for the step_size for each method to reach a specific error
     step_size_eul = 0
@@ -234,17 +266,17 @@ def error_plot(t_values, x_0, ode, exact, args=None):
         error_heun[i] = abs(predict_heun[-1] - exact_value)
 
         # Check if the error from each method is close to the specific error chosen (error_match)
-        if math.isclose(error_match, error_eul[i], abs_tol=1e-6):
+        if math.isclose(error_match, error_eul[i], abs_tol=error_match):
             # Using math.isclose() because it is unlikely for the error to be equal to the error specified
             final_time_eul = time_eul
             step_size_eul = step_sizes[i]
-        if math.isclose(error_match, error_rk4[i], abs_tol=1e-6):
+        if math.isclose(error_match, error_rk4[i], abs_tol=error_match):
             final_time_rk4 = time_rk4
             step_size_rk4 = step_sizes[i]
-        if math.isclose(error_match, error_improv[i], abs_tol=1e-6):
+        if math.isclose(error_match, error_improv[i], abs_tol=error_match):
             final_time_improv = time_improv
             step_size_improv = step_sizes[i]
-        if math.isclose(error_match, error_heun[i], abs_tol=1e-6):
+        if math.isclose(error_match, error_heun[i], abs_tol=error_match):
             final_time_heun = time_heun
             step_size_heun = step_sizes[i]
     print('Time taken by Euler Method: ', final_time_eul)
@@ -271,12 +303,23 @@ def error_plot(t_values, x_0, ode, exact, args=None):
 
 def plot_approx(t_values, x_values, step_size, ode, exact, args=None):
     """
-    Plots the numerical solutions from the numerical methods.
+    Plots the numerical solutions from the different one-step integration methods. Also plots x against dx/dt to
+    illustrate that the Forward Euler method is poor at solving second-order ODEs.
 
     Parameters:
-        t_values        - The range of values of the independent variable
-        x_values        - The initial values of the dependant variables
-        step_size       - The step-size of the numerical methods to be executed
+    ----------
+        t_values : array
+            values of the independent variable
+        x_values : numpy array
+            values of the dependant variable(s)
+        step_size : float
+            The step-size of a single Heun's method step to be executed
+        ode : function
+            The ODE to be integrated
+        exact : function
+            The exact function which returns the true solution
+        args : numpy array
+            The parameters of the ODE
     """
 
     if args is None:  # args cannot be passed through as None because this induces an error when trying to unpack
@@ -316,16 +359,9 @@ def plot_approx(t_values, x_values, step_size, ode, exact, args=None):
 
 
 if __name__ == '__main__':
-    # Plots the errors of each one-step integration method and prints the time take for each method to reach an accuracy
-    # of 1e-6.
-    error_plot([0, 1], 1, ode_first_order, exponential)
-
-    '''
-    Time taken by Euler Method:  0.24495746399999874
-    Time taken by 4th Order Runge Kutta Method:  0.0001070120000008501
-    Time taken by Improved Euler Method:  0.0010338849999982358
-    Time taken by Heun Method:  9.856900000215774e-05
-    '''
+    # Plots the errors of each one-step integration method and prints the time taken for each method and the step-size
+    # required to reach an accuracy of 1e-6.
+    error_plot([0, 1], 1, ode_first_order, exponential, 1e-6)
 
     # Plots the solutions of the ode from the different integration methods and plots the limitation in the use of
     # Euler's method.

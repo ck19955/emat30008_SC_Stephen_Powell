bd69a46801e84acd73ad9850843fe0fd89cb430e
Stephen <ck19955@bristol.ac.uk>
2022-04-29 01:14:17 +0100

    Added docstrings to each function and commented code

--- numerical_shooting.py
+++ numerical_shooting.py
@@ -10,10 +10,19 @@ from function_examples import pred_prey, hopf_bif
 
 def isolate_orbit(ode_data, time_data):
     """
-    :param ode_data: Data created by an ODE solver (currently set as RK4)
-    :param time_data: A linspace of time which the function will search over to find an orbit
-    :return: If an orbit is found returns [initial x value, initial y value, time period] for the orbit
-            else returns None
+    Finds the initial conditions and time period of an orbit using data from a solved ODE
+
+    Parameters:
+    ----------
+        ode_data : array
+            Data created by an ODE solver (currently set as RK4 for accuracy)
+        time_data : array
+             Time data which the function will search over to find an orbit
+
+    Returns:
+    ----------
+        solution_array : list
+            The initial conditions and time period of the orbit found
     """
     x_data = ode_data[:, 0]
     y_data = ode_data[:, 1]
@@ -24,7 +33,8 @@ def isolate_orbit(ode_data, time_data):
         if previous_value:
             if math.isclose(x_data[i], previous_value, abs_tol=1e-4):
                 period = time_data[i] - previous_time
-                return [x_data[i], y_data[i], period]
+                solution_array = [x_data[i], y_data[i], period]
+                return solution_array
         previous_value = x_data[i]
         previous_time = time_data[i]
     raise RuntimeError("No orbit found")
@@ -32,11 +42,28 @@ def isolate_orbit(ode_data, time_data):
 
 def shooting_conditions(ode, u0, pseudo, orbit, args):
     """
-    :param ode: ODE to find orbit for
-    :param u0: initial conditions
-    :param args: Arguments for the ODE
-    :return: The augmented equation suitable for fsolve()
+    Creates the augmented equation containing the conditions necessary to find an orbit of the ODE.
+
+    Parameters:
+    ----------
+        ode : function
+            ODE to find an orbit for
+        u0 : array
+            Current guess for the initial conditions of the orbit
+        pseudo : list
+            A list comprising of the parameters for numerical shooting when using pseudo-arclength
+        orbit : Boolean
+            Decides whether an orbit is being found or not
+        args : numpy array
+            The parameters of the ODE
+
+
+    Returns:
+    ----------
+        g_condition : numpy array
+            Contains the augmented equation to solve for. It is suitable for fsolve()
     """
+
     x0 = u0[:-1]  # Define independent variables
     t0 = u0[-1]
     sol = solve_ivp(ode, (0, t0), x0, max_step=1e-2, args=args)  # Solve ODE
@@ -46,67 +73,118 @@ def shooting_conditions(ode, u0, pseudo, orbit, args):
     return g_condition
 
 
-def pseudo_arclength_conditions(ode, u0, pseudo, orbit, args):
+def pseudo_arclength_conditions(ode, u0, pseudo_info, orbit, args):
+    """
+    Creates the augmented equation containing the conditions necessary to find an orbit of the ODE when using
+    pseudo-arclength continuation.
+
+    Parameters:
+    ----------
+        ode : function
+            ODE to find an orbit for
+        u0 : array
+            Current guess for the initial conditions of the orbit
+        pseudo_info : list
+            A list comprising of the parameters for numerical shooting when using pseudo-arclength
+        orbit : Boolean
+            Decides whether an orbit is being found or not
+        args : numpy array
+            The parameters of the ODE
+
+
+    Returns:
+    ----------
+        g_condition : numpy array
+            Contains the augmented equation to solve for. It is suitable for fsolve()
+    """
+
     # Unpack pseudo argument
-    state_prediction, state_secant, param_prediction, param_secant = pseudo[1:]
+    state_prediction, state_secant, param_prediction, param_secant = pseudo_info[1:]
     if orbit:
         x0 = u0[:-2]  # Define independent variables
         t0 = u0[-2]
         vary_par = u0[-1]
-        args[pseudo[0]] = vary_par
+        args[pseudo_info[0]] = vary_par
         sol = solve_ivp(ode, (0, t0), x0, max_step=1e-2, args=args)  # Solves ODE
 
         x_condition = x0 - sol.y[:, -1]
         t_condition = np.asarray(ode(t0, x0, *args)[0])  # Phase condition
         # Pseudo condition
-        pseudo = np.dot(u0[:-1] - state_prediction, state_secant) + np.dot(vary_par - param_prediction,
-                                                                           param_secant)
-        g_condition = np.concatenate((x_condition, t_condition, pseudo), axis=None)  # Group conditions together
+        pseudo_info = np.dot(u0[:-1] - state_prediction, state_secant) + np.dot(vary_par - param_prediction,
+                                                                                param_secant)
+        g_condition = np.concatenate((x_condition, t_condition, pseudo_info), axis=None)  # Group conditions together
     else:
         x0 = u0[:-1]  # Define independent variables
         vary_par = u0[-1]
-        args[pseudo[0]] = vary_par
+        args[pseudo_info[0]] = vary_par
         x_condition = np.asarray(ode(0, x0, *args)[0])
         # Pseudo condition
-        pseudo = np.dot(u0[:-1] - state_prediction, state_secant) + np.dot(vary_par - param_prediction,
-                                                                           param_secant)
-        g_condition = np.concatenate((x_condition, pseudo), axis=None)  # Group conditions together
+        pseudo_info = np.dot(x0 - state_prediction, state_secant) + np.dot(vary_par - param_prediction, param_secant)
+        g_condition = np.concatenate((x_condition, pseudo_info), axis=None)  # Group conditions together
     return g_condition
 
 
-def shooting(ode, u0, conditions, pseudo, orbit, args):
+def shooting(ode, u0, conditions, pseudo_info, orbit, args):
     """"
-    A function that uses numerical shooting to find limit cycles of a specified ODE.
+    Uses numerical shooting to find a limit cycle of a specified ODE.
 
-    Parameters
-    ode : function
-        The ODE to apply shooting to. The ode function should take arguments for the independent variable, dependant
-        variable and constants, and return the right-hand side of the ODE as a numpy.array.
-    u0 : numpy.array
-        An initial guess at the initial values for the limit cycle.
+    Parameters:
+    ----------
+        ode : function
+            ODE to find an orbit for
+        u0 : array
+            Current guess for the initial conditions of the orbit
+        conditions : function
+            The type of conditions required for the task (normal or pseudo-arclength)
+        pseudo_info : list
+            A list comprising of the parameters for numerical shooting when using pseudo-arclength
+        orbit : Boolean
+            Decides whether an orbit is being found or not
+        args : numpy array
+            The parameters of the ODE
 
-    Returns
-    Returns a numpy.array containing the corrected initial values
-    for the limit cycle. If the numerical root finder failed, the
-    returned array is empty.
+
+    Returns:
+    ----------
+        final : numpy array
+            Contains the initial values for the limit cycle
     """
-    final = fsolve(lambda x: conditions(ode, x, pseudo, orbit, args=args), u0)
+
+    final = fsolve(lambda x: conditions(ode, x, pseudo_info, orbit, args=args), u0)
     return final
 
 
 def plot_function(ode, u0, time_range, step_size, solver, args):
     """
-    :param u0: Initial values
-    :param step_size: maximum step-size for the solver
-    :param solver: chosen solver (not yet implemented, currently set RK4 as default)
-    :return: Plot of the isolated orbit found by the numerical shooter
+    Plots the solution of the ODE over a desired range of time
+
+    Parameters:
+    ----------
+        ode : function
+            ODE to find an orbit for
+        u0 : array
+            Current guess for the initial conditions of the orbit
+        time_range : tuple
+            The range of time to plot over
+        step_size : float
+            Maximum step-size for the solver
+        solver : function
+            Method to solve the ODE
+        args : numpy array
+            The parameters of the ODE
     """
-    x0 = u0[:-1]
-    t0 = u0[-1]
+
+    x0 = u0[:-1]  # Remove the time period from the initial condition of the orbit
     times = np.linspace(0, time_range, num=1000)
+
+    # Solve the ODE using the initial conditions
     data_values = np.asarray(solve_ode(times, x0, step_size, solver, ode, args))
-    plt.plot(times, data_values[:, 0])
-    plt.plot(times, data_values[:, 1])
+
+    # Plot the values
+    for i in range(len(data_values[0])):
+        plt.plot(times, data_values[:, i])
+    # Using for loop so that the function is general and works for different dimensions
+
     plt.xlabel('Time', fontsize=14)
     plt.ylabel('u', fontsize=14)
     plt.yticks(fontsize=12)
@@ -115,29 +193,6 @@ def plot_function(ode, u0, time_range, step_size, solver, args):
 
 
 if __name__ == '__main__':
-
-    '''
-    # args = np.array([1, -1])
-    
-
-    # Plots the solved ODE
-    args = np.array([1, 0.2, 0.1])
-    times1 = np.linspace(0, 400, num=1000)
-    # RK4_values = np.asarray(solve_ode(times1, np.array([0.9, 0]), 0.1, RK4, hopf_bif, args))
-    RK4_values = np.asarray(solve_ode(times1, np.array([1, 1]), 0.1, RK4, pred_prey, args))
-    init_vals = isolate_orbit(RK4_values, times1)
-    print(init_vals)
-    plot_function(pred_prey, init_vals, 400, 0.1, RK4, args)
-
-
-    args = np.array([0.04])
-    #print(shooting(hopf_bif, np.array([1, 1, 8]), shooting_conditions, False, False, args))
-    # Plots the solved ODE
-    times = np.linspace(0, 400, num=1000)
-    # RK4_values = np.asarray(solve_ode(times1, np.array([0.9, 0]), 0.1, rk4, hopf_bif, args))
-    RK4_values = np.asarray(solve_ode(times, np.array([0.3, 0.1]), 0.1, rk4, hopf_bif, args))
-    init_vals = isolate_orbit(RK4_values, times)
-    plot_function(hopf_bif, shooting(hopf_bif, np.array([-0.2, 0, 6]), shooting_conditions, False, True, args), 10, 0.1, rk4, args)
 
     # Plots the solved ODE with varying b values
     fig, axs = plt.subplots(2, 2)
@@ -165,9 +220,9 @@ if __name__ == '__main__':
     for ax in axs.flat:
         ax.label_outer()
     plt.show()
-'''
+
     # Plot solution of the predator-prey model
-    args = np.array([1, 0.4, 0.1])
+    args = np.array([1, 0.2, 0.1])
     times1 = np.linspace(0, 400, num=1000)
     RK4_values = np.asarray(solve_ode(times1, np.array([1, 1]), 0.1, rk4, pred_prey, args))
     plot_function(pred_prey, np.array([1, 1, 20]), 200, 0.1, rk4, args)
